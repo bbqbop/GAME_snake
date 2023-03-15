a7ff348c6c73d333c091ff363a38d9ad6f88a1c8
bbqbop <dierk.peters@gmail.com>
2023-03-15 02:05:59 -0400

    fix food positioning

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let snake = '>'
 let food = '0';
-const body = document.querySelector('#body');
+const field = document.querySelector('#field');
+let fieldWidth = Number((getComputedStyle(field).width).split('').slice(0,-2).join(''))
+let fieldHeight = Number((getComputedStyle(field).height).split('').slice(0,-2).join(''))
 const snakeDisplay = document.createElement('p');
 const foodDisplay = document.createElement('p');
 let interval;
-snakeDisplay.innerText = snake
+snakeDisplay.innerText = '>'
 snakeDisplay.classList.add('snake');
 foodDisplay.innerText = food;
 foodDisplay.classList.add('food');
@@ -47,15 +48,19 @@ function changeDirection (dir){
     interval = setInterval(()=>{
         switch(dir){
             case('right'): 
+            snakeDisplay.style.cssText = 'transform: rotate(0deg)'
                 newLeft += 10;
                 break;
             case('left'):
+                snakeDisplay.style.cssText = 'transform: rotate(180deg)';
                 newLeft -= 10;
                 break;
             case('up'):
+                snakeDisplay.style.cssText = 'transform: rotate(270deg)'
                 newTop -= 10;
                 break;
             case('down'):
+            snakeDisplay.style.cssText = 'transform: rotate(90deg)'
                 newTop += 10;
                 break;
         }
@@ -64,17 +69,15 @@ function changeDirection (dir){
         belly.push(document.createElement('p'));
         belly[belly.length-1].innerText = '0';
         belly[belly.length-1].classList.add('belly');
-        body.append(belly[belly.length-1]);
+        field.append(belly[belly.length-1]);
     }
     if(belly.length > 0){
         for(let i = belly.length-1; i >= 0; i--){
-            console.log(i);
             if(i === 0){
                 belly[i].style.left = `${posLeft}px`;
                 belly[i].style.top = `${posTop}px`;
             } 
             else{ 
-                console.log(i, belly[i]);
                 belly[i].style.left = belly[i-1].style.left
                 belly[i].style.top = belly[i-1].style.top
             }
@@ -89,14 +92,21 @@ function changeDirection (dir){
 }
 
 function setFood(){
-    foodLeft = (Math.floor((window.innerWidth * Math.random()) / 10)) * 10
-    foodTop = (Math.floor((window.innerHeight * Math.random()) / 10)) * 10
+    foodLeft = (Math.floor((fieldWidth * Math.random()) / 10)) * 10 + 20;
+    foodTop = (Math.floor((fieldHeight * Math.random()) / 10)) * 10 + 20;
+    if(foodLeft >= fieldWidth - 40){
+        foodLeft -= 20
+    }
+    if(foodTop >= fieldWidth - 40){
+        foodTop -= 20
+    }
+    
     foodDisplay.style.left = `${foodLeft}px`
     foodDisplay.style.top = `${foodTop}px`
 }
 
 setFood(); 
-body.append(snakeDisplay, foodDisplay)
+field.append(snakeDisplay, foodDisplay)
 
 
  
