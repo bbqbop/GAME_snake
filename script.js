a2116aa725989a077ea1264ff0adf13b920aa3d4
bbqbop <dierk.peters@gmail.com>
2023-03-15 13:48:46 -0400

    working on gameOver, trying to stop movement

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const field = document.querySelector('#field');
 const snakeDisplay = document.createElement('p');
 const foodDisplay = document.createElement('p');
+const handleKeyDown = function(event) {
+    controlKeys(event);
+}
 snakeDisplay.innerText = '>'
 foodDisplay.innerText = '0';
 snakeDisplay.classList.add('snake');
@@ -17,28 +20,17 @@ let foodLeft;
 let foodTop;
 let belly = [];
 let interval;
+let gameEnd = false;
 
-document.body.addEventListener('keydown', function(e){
-    if(e.key === 'Escape') {
-        gameOver()
-    }
-    if(e.key === 'ArrowRight'){
-        moveSnake('right')
-    }
-    if(e.key === 'ArrowLeft'){
-        moveSnake('left')
-    }
-    if(e.key === 'ArrowDown'){
-        moveSnake('down')
-    }
-    if(e.key === 'ArrowUp'){
-        moveSnake('up')
-    }
-})
+document.addEventListener('keydown', handleKeyDown)
 function moveSnake (dir){
     clearInterval(interval)
     interval = setInterval(()=>{
         changeDirection(dir);
+        if (gameEnd){
+            gameOver()
+            return;
+        }
         if(posLeft === foodLeft && posTop === foodTop){
             eat();
         }
@@ -63,7 +55,8 @@ function moveSnake (dir){
         posTop = newTop;
         snakeDisplay.style.left = `${posLeft}px`;
         snakeDisplay.style.top = `${posTop}px`;
-        },50);
+        }
+    ,50);
 }
 function setFood(){
     foodLeft = (Math.floor((window.innerWidth * Math.random()) / 10)) * 10 + 20;
@@ -80,29 +73,47 @@ function setFood(){
 }
 function gameOver(){
     clearInterval(interval);
+    document.removeEventListener('keydown', handleKeyDown)
     alert('gameOver')
+}
+function controlKeys(e){
+    if(e.key === 'Escape') {
+        gameOver()
+    }
+    if(e.key === 'ArrowRight'){
+        moveSnake('right')
+    }
+    if(e.key === 'ArrowLeft'){
+        moveSnake('left')
+    }
+    if(e.key === 'ArrowDown'){
+        moveSnake('down')
+    }
+    if(e.key === 'ArrowUp'){
+        moveSnake('up')
+    }
 }
 function changeDirection(dir){
     switch(dir){
         case('right'): 
         snakeDisplay.style.cssText = 'transform: rotate(0deg)'
             newLeft += 10;
-            if(newLeft >= window.innerWidth - 20) gameOver();
+            if(newLeft >= window.innerWidth - 20) gameEnd = true;
             break;
         case('left'):
             snakeDisplay.style.cssText = 'transform: rotate(180deg)';
             newLeft -= 10;
-            if(newLeft <= 10) gameOver();
+            if(newLeft <= 10) gameEnd = true;
             break;
         case('up'):
             snakeDisplay.style.cssText = 'transform: rotate(270deg)'
             newTop -= 10;
-            if(posTop <= 0) gameOver();
+            if(posTop <= 0) gameEnd = true;
             break;
         case('down'):
         snakeDisplay.style.cssText = 'transform: rotate(90deg)'
             newTop += 10;
-            if(newTop >= window.innerHeight - 20) gameOver();
+            if(newTop >= window.innerHeight - 20) gameEnd = true;
             break;
     }
 }
